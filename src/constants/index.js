d3afd242f77057441da07f9fcbf9092a62d849c6
Heber Pereira <isaiahpereira88@gmail.com>
2023-08-28 20:11:23 -0700

    complete service section

--- src/constants/index.js
+++ src/constants/index.js
@@ -85,17 +85,20 @@ export const products = [
 
 export const services = [
   {
+    key: 1,
     imgURL: truckFast,
     label: "Free shipping",
     subtext: "Enjoy seamless shopping with our complimentary shipping service.",
   },
   {
+    key: 2,
     imgURL: shieldTick,
     label: "Secure Payment",
     subtext:
       "Experience worry-free transactions with our secure payment options.",
   },
   {
+    key: 3,
     imgURL: support,
     label: "Love to help you",
     subtext: "Our dedicated team is here to assist you every step of the way.",
