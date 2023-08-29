d45eaf931a081359924c27fbc00983d56a849c12
Heber Pereira <isaiahpereira88@gmail.com>
2023-08-28 20:31:46 -0700

    completed special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,20 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({
+  label,
+  iconUrl,
+  backgroundColor,
+  borderColor,
+  textColor,
+}) => {
   return (
     <button
       className={`
         flex justify-center items-center gap-2 
-        px-7 py-4 border border-coral-red font-montserrat text-lg 
-        leading-none bg-coral-red rounded-full text-white
+        px-7 py-4 border rounded-full  font-montserrat text-lg 
+        leading-none ${
+          backgroundColor
+            ? `${backgroundColor} ${textColor} ${borderColor}`
+            : "bg-coral-red text-white border-coral-red"
+        } 
       `}
     >
       {label}
