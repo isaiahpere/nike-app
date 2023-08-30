911cf0bda5a315630f3ec258d50aa346c4822e86
Heber Pereira <isaiahpereira88@gmail.com>
2023-08-29 20:03:21 -0700

    completed subscribe section

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,5 +1,26 @@
+import Button from "./Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container flex justify-between items-center flex-col gap-10"
+      id="contact-us"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-lg font-palanquin font-bold text-center">
+        Sign Up from
+        <span className="text-coral-red"> Updates</span> & Newsletter
+      </h3>
+      <div
+        className={`lg:max-w-[50%] w-full flex items-center max-sm:flex-col gap-5 
+        p-2.5 sm:border sm:border-slate-gray rounded-full`}
+      >
+        <input type="text" placeholder="Subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullwidth />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
